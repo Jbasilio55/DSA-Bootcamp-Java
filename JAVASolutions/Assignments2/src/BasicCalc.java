abaed62f397200b92cd033c528e6e0245c9e679c
Jbasilio55 <j.l.basilio55@gmail.com>
2025-08-23 21:08:48 -0400

    Assignment 4 complete (I/O)

--- JAVASolutions/Assignments2/src/BasicCalc.java
+++ JAVASolutions/Assignments2/src/BasicCalc.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class BasicCalc {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("What is your first number?");
+        int firstNum = sc.nextInt();
+        System.out.println("What is your second number?");
+        int secondNum = sc.nextInt();
+        System.out.println("What is your operator add(+), minus(-), multiply(*), divide(/)?: ");
+        sc.nextLine();
+        String operator = sc.nextLine();
+
+        int answer;
+
+        if(operator.equals("+")){
+            answer = firstNum + secondNum;
+            System.out.println("Answer: "+ answer);
+        } else if (operator.equals("-")) {
+            answer = firstNum - secondNum;
+            System.out.println("Answer: "+ answer);
+        } else if (operator.equals("*")) {
+            answer = firstNum * secondNum;
+            System.out.println("Answer: "+ answer);
+        }else{
+            if(secondNum == 0){
+                System.out.println("You can not divide by zero");
+            }else{
+                answer = firstNum / secondNum;
+                System.out.println("Answer: "+ answer);
+            }
+        }
+
+        sc.close();
     }
 }
